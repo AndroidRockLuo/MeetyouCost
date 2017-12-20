4d6c274c187437212c6cf16e55a508890f5616e8
linwuhan <452825089@qq.com>
2017-12-20 13:49:12 +0800

    support aar cost

--- meetyoucostplugin/src/main/groovy/com.meiyou.meetyoucostplugin/MeetyouCostPluginImp.groovy
+++ meetyoucostplugin/src/main/groovy/com.meiyou.meetyoucostplugin/MeetyouCostPluginImp.groovy
@@ -5,11 +5,18 @@ import com.android.build.gradle.AppExtension
 import com.android.build.gradle.internal.pipeline.TransformManager
 import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Opcodes
+
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
+import java.util.jar.JarOutputStream
+import java.util.zip.ZipEntry
 
 import static org.objectweb.asm.ClassReader.EXPAND_FRAMES
 
@@ -47,6 +54,7 @@ public class MeetyouCostPluginImp extends Transform implements Plugin<Project> {
     public boolean isIncremental() {
         return false;
     }
+
     @Override
     void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs,
                    TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
@@ -67,16 +75,16 @@ public class MeetyouCostPluginImp extends Transform implements Plugin<Project> {
                                 if (name.endsWith(".class") && !name.startsWith("R\$") &&
                                         !"R.class".equals(name) && !"BuildConfig.class".equals(name)) {
                                     ClassReader classReader = new ClassReader(file.bytes)
-                                    ClassWriter classWriter = new ClassWriter(classReader,ClassWriter.COMPUTE_MAXS)
+                                    ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS)
                                     def className = name.split(".class")[0]
-                                    ClassVisitor cv = new CostMethodClassVisitor(className,classWriter)
+                                    ClassVisitor cv = new CostMethodClassVisitor(className, classWriter)
                                     classReader.accept(cv, EXPAND_FRAMES)
                                     byte[] code = classWriter.toByteArray()
                                     FileOutputStream fos = new FileOutputStream(
                                             file.parentFile.absolutePath + File.separator + name)
                                     fos.write(code)
                                     fos.close()
-                                    CostMethodClassVisitor
+
                                 }
                                 //println '//MeetyouCostPluginImp find file:' + file.getAbsolutePath()
                                 //project.logger.
@@ -90,10 +98,10 @@ public class MeetyouCostPluginImp extends Transform implements Plugin<Project> {
             }
 
 
-            input.jarInputs.each { JarInput jarInput ->
-                /**
+            /*input.jarInputs.each { JarInput jarInput ->
+                *//**
                  * 重名名输出文件,因为可能同名,会覆盖
-                 */
+                 *//*
                 def jarName = jarInput.name
                 def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
                 if (jarName.endsWith(".jar")) {
@@ -107,6 +115,96 @@ public class MeetyouCostPluginImp extends Transform implements Plugin<Project> {
                         jarInput.contentTypes, jarInput.scopes, Format.JAR)
 
                 FileUtils.copyFile(jarInput.file, dest)
+
+            }
+            */
+
+            input.jarInputs.each { JarInput jarInput ->
+                /**
+                 * 重名名输出文件,因为可能同名,会覆盖
+                 */
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+
+                File tmpFile = null;
+                if (jarInput.file.getAbsolutePath().endsWith(".jar")) {
+                    JarFile jarFile = new JarFile(jarInput.file);
+                    Enumeration enumeration = jarFile.entries();
+                    tmpFile = new File(jarInput.file.getParent() + File.separator + "classes_meetyoucost.jar");
+                    //避免上次的缓存被重复插入
+                    if (tmpFile.exists()) {
+                        tmpFile.delete();
+                    }
+                    JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(tmpFile));
+                    //用于保存
+                    ArrayList<String> processorList = new ArrayList<>();
+                    while (enumeration.hasMoreElements()) {
+                        JarEntry jarEntry = (JarEntry) enumeration.nextElement();
+                        String entryName = jarEntry.getName();
+                        ZipEntry zipEntry = new ZipEntry(entryName);
+                        //println "MeetyouCost entryName :" + entryName
+                        InputStream inputStream = jarFile.getInputStream(jarEntry);
+                        //如果是inject文件就跳过
+
+                        //anna插桩class
+                        if (entryName.endsWith(".class") && !entryName.contains("R\$") &&
+                                !entryName.contains("R.class") && !entryName.contains("BuildConfig.class")) {
+                            //class文件处理
+                            jarOutputStream.putNextEntry(zipEntry);
+                            ClassReader classReader = new ClassReader(IOUtils.toByteArray(inputStream))
+                            ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS)
+                            def className = name.split(".class")[0]
+                            ClassVisitor cv = new CostMethodClassVisitor(className,classWriter)
+                            classReader.accept(cv, EXPAND_FRAMES)
+                            byte[] code = classWriter.toByteArray()
+                            jarOutputStream.write(code)
+
+                        }   else if (entryName.contains("META-INF/services/javax.annotation.processing.Processor")) {
+                            if (!processorList.contains(entryName)) {
+                                processorList.add(entryName)
+                                jarOutputStream.putNextEntry(zipEntry);
+                                jarOutputStream.write(IOUtils.toByteArray(inputStream));
+                            } else {
+                                println "duplicate entry:" + entryName
+                            }
+                        } else {
+
+                            jarOutputStream.putNextEntry(zipEntry);
+                            jarOutputStream.write(IOUtils.toByteArray(inputStream));
+                        }
+
+                        jarOutputStream.closeEntry();
+                    }
+                    //写入inject注解
+
+                    //写入inject文件
+//                    ZipEntry addEntry = new ZipEntry(injectClazz + ".class");
+//                    jarOutputStream.putNextEntry(addEntry);
+//                    jarOutputStream.write(annaInjectWriter.inject(injectClazz,false));
+//                    jarOutputStream.closeEntry();
+//
+//                    clazzindex++ ;
+                    //结束
+                    jarOutputStream.close();
+                    jarFile.close();
+//                    jarInput.file.delete();
+//                    tmpFile.renameTo(jarInput.file);
+                }
+//                println 'Assassin-----> find Jar:' + jarInput.getFile().getAbsolutePath()
+
+                //处理jar进行字节码注入处理 TODO
+
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                if (tmpFile == null) {
+                    FileUtils.copyFile(jarInput.file, dest)
+                } else {
+                    FileUtils.copyFile(tmpFile, dest)
+                    tmpFile.delete()
+                }
             }
         }
         println '//===============MeetyouCostPluginImp visit end===============//'
